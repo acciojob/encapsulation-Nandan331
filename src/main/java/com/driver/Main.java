a508bd0ab81fd8618d08c7e3656838bed1c27795
Nandan331 <nanduumanyala@gmail.com>
2026-09-17 22:10:31 +0530

    first commit on encapsulation code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        obj.name = "Nandan";
+        System.out.println(obj.name);
+//        'name' has private access in com.driver.RWOnly
+
+        obj.setName("Nandan");
+        System.out.println(obj.getName());
+
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    String n;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
